eee90fcd42936e9c718ee4d85abe95db0241961b
arimohy <arimohy@gmail.com>
2021-09-09 07:50:33 -0500

    Ejercicio 5 terminado en clase Punto5

--- src/Punto5.java
+++ src/Punto5.java
@@ -1,2 +1,19 @@
 public class Punto5 {
+    public static void ParesImpares(){
+        int c=1;
+        String pares="Los numeros pares son: ";
+        String impares="Los numeros impares son: ";
+        while (c<=100){
+            if(c%2==0)
+                pares+=String.valueOf(c)+",";
+            else
+                impares+=String.valueOf(c)+",";
+            c+=1;
+        }
+        System.out.println(pares);
+        System.out.println(impares);
+    }
+    public static void main(String[] args) {
+        ParesImpares();
+    }
 }
