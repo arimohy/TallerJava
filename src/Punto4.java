acb2aa1458b8720dcdd73b01ea02f0ca111661c2
arimohy <arimohy@gmail.com>
2021-09-09 07:29:31 -0500

    Ejercicio 4 terminado en clase Punto4

--- src/Punto4.java
+++ src/Punto4.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class Punto4 {
+    //CALCULAR PRECIO FINAL
+
+    public static void preciofinal(){
+        double IVA=0.21;
+        Scanner escaner = new Scanner (System.in); //Creación de un objeto Scanner
+        System.out.println ("Ingrese el precio del producto:");
+        double precioproducto = Double.parseDouble(escaner.nextLine());
+        double preciofinal=precioproducto+precioproducto*IVA;
+
+    }
+    public static void main(String[] args) {
+        preciofinal();
+    }
 }
