3935805c67567376ef02415dd48fbf1b51f4beed
arimohy <arimohy@gmail.com>
2021-09-09 06:55:52 -0500

    ejercicio 3 completadoy y creacion de clase ejecicio 4

--- src/Punto3.java
+++ src/Punto3.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Punto3 {
+import java.util.Scanner;
+
+public class Punto3 {
+    static void areaCirculo(){
+        Scanner escaner = new Scanner (System.in); //Creación de un objeto Scanner
+
+        System.out.println ("Ingrese el radio del circulo:");
+        double radio = Double.parseDouble(escaner.nextLine());
+        double area=Math.PI*Math.pow(radio,2);
+        System.out.println ("El area del circulo es:");
+        System.out.println(area);
+
+    }
+
+    public static void main(String[] args) {
+        areaCirculo();
+
+    }
 }
