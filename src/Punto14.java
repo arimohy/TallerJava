eb65e5b9faf34c1ac22eecaee14e6ac62a3e16a4
arimohy <arimohy@gmail.com>
2021-09-09 14:31:07 -0500

    Ejercicio 14 terminado en clase Punto14

---
+++ src/Punto14.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Punto14 {
+    public static void salto2por2(){
+        /*Crear un programa que pida un numero por teclado y que imprima por pantalla los números desde
+         el numero introducido hasta 1000 con saldos de 2 en 2.*/
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("==========SALTOS DE 2 EN 2 HASTA 1000===========");
+        System.out.println ("Ingrese el numero del que quiere comenzar a saltar :");
+        int nro=scanner.nextInt();
+        while (nro<=1000){
+            System.out.println(nro);
+            nro+=2;
+        }
+
+    }
+    public static void main(String[] args) {
+        salto2por2();
+    }
+}
