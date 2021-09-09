071c4ddc0c3a79a5629869ba2b937df6bd2907ab
arimohy <arimohy@gmail.com>
2021-09-09 11:34:40 -0500

    Ejercicio 10 terminado en clase Punto10

--- src/Punto10.java
+++ src/Punto10.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class Punto10 {
+    public static void quitarespacios(){
+        System.out.println ("Ingrese una frase para eliminar espacios :");
+        Scanner scanner=new Scanner(System.in);
+        String cadena=scanner.nextLine();
+        cadena=cadena.replace(" ","");
+        System.out.println("La frase quedo asi:");
+        System.out.println(cadena);
+    }
+    public static void main(String[] args) {
+        quitarespacios();
+    }
 }
