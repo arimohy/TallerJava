470844bd279ff65280ac365d46569bb0d0073acb
arimohy <arimohy@gmail.com>
2021-09-09 11:26:35 -0500

    Ejercicio 9 terminado en clase Punto9

---
+++ src/Punto9.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Punto9 {
+    public static void frase(){
+        /*Del texto, “La sonrisa sera la mejor arma contra la tristeza” Reemplaza todas las a del String anterior
+        por una e, adicionalmente concatenar a esta frase una adicional que ustedes quieran incluir por consola y
+         las muestre luego unidas.*/
+
+        String cadena="La sonrisa sera la mejor arma contra la tristeza";
+        cadena=cadena.replace('a','e');
+        Scanner scanner=new Scanner(System.in);
+        System.out.println ("Ingrese una frase que quiera agregar :");
+        cadena+=", "+scanner.nextLine();
+        System.out.println("La frase quedo asi:");
+        System.out.println(cadena);
+    }
+    public static void main(String[] args) {
+        frase();
+    }
+}
