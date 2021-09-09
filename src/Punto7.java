0710cf8e5ea5dbcbc8f2df73c2e53cc21b993133
arimohy <arimohy@gmail.com>
2021-09-09 10:44:02 -0500

    Ejercicio 7 terminado en clase Punto7

---
+++ src/Punto7.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Punto7 {
+    public static  void mayoracero(){
+        double nro;
+        do{
+            Scanner escaner = new Scanner (System.in); //Creación de un objeto Scanner
+            System.out.println ("Ingrese un numero:");
+            nro = Double.parseDouble(escaner.nextLine());
+        }while (nro<0);
+        System.out.println("El numero mayor a cero es : "+nro);
+
+    }
+    public static void main(String[] args) {
+        mayoracero();
+    }
+}
