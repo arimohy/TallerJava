6d086b1d5acf29a0f26dbf8127c0a0c5a555c53e
arimohy <arimohy@gmail.com>
2021-09-09 14:19:44 -0500

    Ejercicio 13 terminado en clase Punto13

---
+++ src/Punto13.java
@@ -0,0 +1,13 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Punto13 {
+    public static void consultarfecha(){
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("(yyyy/MM/dd) (HH:mm:ss)");
+        System.out.println("La fecha actual es:-> "+dtf.format(LocalDateTime.now()));
+
+    }
+    public static void main(String[] args) {
+        consultarfecha();
+    }
+}
